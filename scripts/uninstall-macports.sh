65786680d1f854fcb7b65b0a5d8725fdfa819d66
Jimmy Tang <jtang@tchpc.tcd.ie>
2014-01-06 11:00:38 +0000

    uninstall macports

---
+++ scripts/uninstall-macports.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo port installed > installed.txt
+sudo port -fp uninstall installed
+sudo rm -rf \
+    /opt/local \
+    /Applications/DarwinPorts \
+    /Applications/MacPorts \
+    /Library/LaunchDaemons/org.macports.* \
+    /Library/Receipts/DarwinPorts*.pkg \
+    /Library/Receipts/MacPorts*.pkg \
+    /Library/StartupItems/DarwinPortsStartup \
+    /Library/Tcl/darwinports1.0 \
+    /Library/Tcl/macports1.0 \
+    ~/.macports
